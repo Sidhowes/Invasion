f678dc6c312c369770c96c1db43eb27d6f6fa431
Sidhowes <sidhowes@mail.com>
2020-08-21 13:24:17 +0000

    implenting flipping cards using javascript.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,56 +1,28 @@
- $("#volume").click(function () {
-        volumeOff();
-    });
+//card game logic
+let flippedCard = false;
+let firstCard, secondCard;
+let gameLock = true;
+let pairs = 6;
+let clicks = 0;
 
-    function volumeOff() {
-        $("#volume").addClass("hide-button");
-        $("#mute").removeClass("hide-button");
-        $("audio").prop("muted", true);
+card.forEach(cards => cards.addEventListener('click', flipOver));
+shuffle();
+
+function flipOver() {
+    if (gameLock) return;
+    if (this === firstCard) return;
+    
+    this.classList.add('visible');
+
+    if (!flippedCard) {
+        // The first click, card
+        flippedCard = true;
+        firstCard = this;
+        
+    } else {
+        // The second click, card
+        flippedCard = false;
+        secondCard = this;
     }
-
-    $("#mute").click(function () {
-        volumeOn();
-    });
-
-    function volumeOn() {
-        $("#volume").removeClass("hide-button");
-        $("#mute").addClass("hide-button");
-        $("audio").prop("muted", false);
-    }
-
-    class Invasion {
-        constructor(totalTime, cards) {
-            this.cardsArray = cards;
-            this.totalTime = totalTime;
-            this.timeRemaining = totalTime;
-            this.timer = document.getElementById("time-remaining");
-            this.ticker = document.getElementById("flips");
-        }
-        startGame() {
-            this.cardToCheck = null;
-            this.totalClicks = 0;
-            this.timeRemaining = this.totalTime;
-            this.matchedCards = [];
-            this.busy = true;
-        }
-    }
-
-
-
-    function ready() {
-        let overlays = Array.from(document.getElementsByClassName("overlay-text"));
-        let cards = Array.from(document.getElementsByClassName("card"));
-
-        overlays.forEach(overlay => {
-            overlay.addEventListener("click", () => {
-                overlay.classList.remove("visible")
-            });
-        });
-        cards.forEach(card => {
-            card.addEventListener("click", () => {
-                //game.flipcard
-            });
-        });
-
-
-    }
+}
+      
