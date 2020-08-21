d9bbf705e949030f63e3536bf80c7a10ce235abd
Sidhowes <sidhowes@mail.com>
2020-08-21 13:47:10 +0000

    mentor changes

--- assets/js/game.js
+++ assets/js/game.js
@@ -5,21 +5,16 @@ let gameLock = true;
 let pairs = 6;
 let clicks = 0;
 let cards = Array.from(document.getElementsByClassName('card'));
-
-card.forEach(cards => cards.addEventListener('click', flipOver));
-shuffle();
-
+cards.forEach(card => card.addEventListener('click', flipOver));
 function flipOver() {
+    console.log('Inside function');
     if (gameLock) return;
     if (this === firstCard) return;
-    
     this.classList.add('visible');
-
     if (!flippedCard) {
         // The first click, card
         flippedCard = true;
         firstCard = this;
-        
     } else {
         // The second click, card
         flippedCard = false;
