4d26da34a9f2f79ca9a7b8980ad1c961c99cf416
Sidhowes <sidhowes@mail.com>
2020-08-25 12:29:17 +0000

    images replaced and layout of container changed.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,24 +1,7 @@
-//card game logic
-let flippedCard = false;
-let firstCard, secondCard;
-let gameLock = true;
-let pairs = 6;
-let clicks = 0;
-let cards = Array.from(document.getElementsByClassName('card'));
-cards.forEach(card => card.addEventListener('click', flipOver));
-function flipOver() {
-    console.log('Inside function');
-    if (gameLock) return;
-    if (this === firstCard) return;
-    this.classList.add('visible');
-    if (!flippedCard) {
-        // The first click, card
-        flippedCard = true;
-        firstCard = this;
-    } else {
-        // The second click, card
-        flippedCard = false;
-        secondCard = this;
-    }
+const cards = document.querySelectorAll(".card");
+
+function flipcard() {
+    this.classList.toggle("flip");
 }
-      
+
+cards.forEach(card => card.addEventListener("click", flipcard));
