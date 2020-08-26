fb8b3cc641dde2b45d5d62eb1036453e6951856c
Sidhowes <sidhowes@mail.com>
2020-08-26 07:49:48 +0000

    first click and second click javascript implented to flip cards.

--- assets/js/game.js
+++ assets/js/game.js
@@ -1,7 +1,27 @@
 const cards = document.querySelectorAll(".card");
 
+let hasFlippedCard = false;
+let lockBoard = false;
+let firstCard, secondCard;
+
 function flipcard() {
-    this.classList.toggle("flip");
-}
+  if (lockBoard) return;
+  if (this === firstCard) return;
+  this.classList.add("flip");
+
+  if (!hasFlippedCard) {
+      //first click
+      hasFlippedCard = true;
+      firstCard = this;
+
+      return;
+  }
+
+    //second click
+    secondCard = this;
+    
+    
+
+
 
 cards.forEach(card => card.addEventListener("click", flipcard));
